57ae2e7b5c582bba3c75b1b5e85a21df9721e251
Jiri Bucek <jibu@nodesagency.com>
2019-06-25 10:43:47 +0200

    xcodeproj updated

--- Package.swift
+++ Package.swift
@@ -23,9 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Cashier",
-            dependencies: ["objc"]),
+            dependencies: []),
         .testTarget(
             name: "CashierTests",
-            dependencies: ["Cashier", "objc"]),
+            dependencies: ["Cashier"]),
     ]
 )
